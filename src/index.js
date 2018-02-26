2726a372f029adec2fb6ea59cf934bed75146e5c
NastyaMedv <NastyaMedv@gmail.com>
2018-02-26 18:53:39 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    var tri = 0;
+  
+    for (var i=0; i<preferences.length; i++) {
+        var first = i;
+        var second = preferences[first]-1;
+        var third = preferences[second]-1;
+        if (preferences[third]==first+1) {
+            tri++;
+        }
+    }
+    var answer = Math.floor(tri/3);
+    
+    return answer;
 };
